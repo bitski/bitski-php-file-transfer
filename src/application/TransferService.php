dd733340810d2765d614e37d5b66804c352054f2
bitski <18152029+bitski@users.noreply.github.com>
2026-09-08 19:13:28 +0200

    feat(application): add transfer service

---
+++ src/application/TransferService.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Transfer service.
+ *
+ * @since 0.1.0
+ */
+
+namespace BitskiPHPFileTransfer\application;
+
+class TransferService
+{
+    /**
+     * Creates a new transfer.
+     */
+    public function create() {}
+}
