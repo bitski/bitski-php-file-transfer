b312320e530b26206292ca5ee45064cce63f6359
bitski <18152029+bitski@users.noreply.github.com>
2026-09-22 18:02:37 +0200

    feat(domain): implement Transfer entity with properties and accessors

--- src/domain/Transfer.php
+++ src/domain/Transfer.php
@@ -7,4 +7,63 @@
 
 namespace BitskiPHPFileTransfer\domain;
 
-class Transfer {}
+use DateTimeImmutable;
+
+class Transfer
+{
+    protected int $id;
+    protected DateTimeImmutable $createdAt;
+    protected string $initiator;
+    protected string $recipient;
+    protected string $mimeType;
+    protected string $fileName;
+
+    /**
+     * @since 0.1.8
+     */
+    public function __construct(
+        int $id,
+        DateTimeImmutable $createdAt,
+        string $initiator,
+        string $recipient,
+        string $mimeType,
+        string $fileName,
+    ) {
+        $this->id        = $id;
+        $this->createdAt = $createdAt;
+        $this->initiator = $initiator;
+        $this->recipient = $recipient;
+        $this->mimeType  = $mimeType;
+        $this->fileName  = $fileName;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getCreatedAt(): DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function getInitiator(): string
+    {
+        return $this->initiator;
+    }
+
+    public function getRecipient(): string
+    {
+        return $this->recipient;
+    }
+
+    public function getMimeType(): string
+    {
+        return $this->mimeType;
+    }
+
+    public function getFileName(): string
+    {
+        return $this->fileName;
+    }
+}
